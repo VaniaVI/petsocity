15e68c87ce7165846538c18ef8a588889ec6f0b7
VaniaVI <vania.vargas.igor@gmail.com>
2025-11-03 02:19:42 -0300

    Se crea un suspense que sirve para la carga de los datos de producto como un spinner. pero este es necesario por que se utiliza useSearchParams que necesita del cliente

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -2,36 +2,37 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import ProductCard from "../../components/ProductCard";
+import { Suspense } from "react";
 
-export default function Products(){
+function Products() {
     const searchParams = useSearchParams();
     const category = searchParams.get("category");
     const tag = searchParams.get("tag");
 
     const [products, setProducts] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
         async function fetchData() {
             const res = await fetch("./api/products");
             const data = await res.json();
 
             let filtered = data;
 
-            if(category){
+            if (category) {
                 filtered = filtered.filter(prod => prod.categoria === category);
             }
 
-            if(tag){
+            if (tag) {
                 filtered = filtered.filter(prod => prod.etiquetas?.includes(tag));
             }
+
             setProducts(filtered);
         }
-        {/*Funcion para obtener los productos*/}
-        fetchData();
-      {/* si alguno de estos valores cambia, se vuelve a  ejecutar el fetch */}
-    },[category,tag]);
 
-    return(
+        fetchData();
+    }, [category, tag]);
+
+    return (
         <main className="container py-4">
             <header className="text-center mb-4">
                 <h2>Nuestros Productos</h2>
@@ -39,15 +40,20 @@ export default function Products(){
                 {tag && `Etiqueta: ${tag}`}
                 {!category && !tag && "Todos los productos"}
             </header>
-            <nav>
-
-            </nav>
-            {/* Aqui se insertan dinamicamento los productos llamando a la api y al componente de productCard */}
+            <nav></nav>
             <section className="row justify-content-center gap-3">
-                {products.map(prod =>(
-                    <ProductCard key={prod.id} product={prod} className="col-12 col-sm-6 col-md-4 col-lg-3"/>
+                {products.map(prod => (
+                    <ProductCard key={prod.id} product={prod} className="col-12 col-sm-6 col-md-4 col-lg-3" />
                 ))}
             </section>
         </main>
-    )
+    );
+}
+
+export default function ProductsPage() {
+    return (
+        <Suspense fallback={<p className="text-center py-4">Cargando productos...</p>}>
+            <Products />
+        </Suspense>
+    );
 }
